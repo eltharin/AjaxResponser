b05b4b9273f33588aad8b4d33bf7d9f33329a230
eltharin <eltharin18@hotmail.fr>
2023-07-02 18:59:09 +0200

    add target method

--- src/Annotations/AjaxCallOrNot.php
+++ src/Annotations/AjaxCallOrNot.php
@@ -5,7 +5,7 @@ namespace Eltharin\AjaxResponserBundle\Annotations;
 use Attribute;
 use Doctrine\ORM\Mapping\Annotation;
 
-#[Attribute(Attribute::TARGET_FUNCTION)]
+#[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD)]
 class AjaxCallOrNot implements Annotation
 {
 
