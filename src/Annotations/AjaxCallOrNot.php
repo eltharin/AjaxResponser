cdbe21478a97dede9780992f1b89baa86299dd38
eltharin <eltharin18@hotmail.fr>
2023-07-03 15:01:09 +0200

    deprecated corrections

--- src/Annotations/AjaxCallOrNot.php
+++ src/Annotations/AjaxCallOrNot.php
@@ -3,10 +3,10 @@
 namespace Eltharin\AjaxResponserBundle\Annotations;
 
 use Attribute;
-use Doctrine\ORM\Mapping\Annotation;
+use Doctrine\ORM\Mapping\MappingAttribute;
 
 #[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD)]
-class AjaxCallOrNot implements Annotation
+class AjaxCallOrNot implements MappingAttribute
 {
 
 }
